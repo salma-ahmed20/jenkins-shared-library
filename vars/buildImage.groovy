a91f3aa09ec6603097860e5247377d258292af6a
salma <salma.ahmed0075@gmail.com>
2023-04-19 00:36:22 +0200

    updates

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,6 +1,6 @@
 
 def call(){
-    echo 'Building docker image'
+    echo 'Building docker image from shared lib'
     withCredentials([usernamePassword(credentialsId: 'dockerhub-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
         sh 'docker build -t salma101/java-maven-app:jma-2.0 .'
         sh "echo $PASS| docker login -u $USER --password-stdin"
--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,4 +1,4 @@
 def call (){
-    echo 'building the application...'
+    echo 'building the application from shared lib...'
     sh 'mvn package'
 }
