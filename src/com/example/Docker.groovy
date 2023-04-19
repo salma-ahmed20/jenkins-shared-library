d6e10c9e908502c9698463334b21e627e04d62a6
salma <salma.ahmed0075@gmail.com>
2023-04-19 13:58:28 +0200

    updates

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -1,25 +1,26 @@
 package com.example
 
-class Docker implements Serializable{
+class Docker implements Serializable {
 
     def script
 
-    Docker(script){
+    Docker(script) {
         this.script = script
     }
 
-    def buildImage(String imageName){
+    def buildImage(String imageName) {
         script.echo 'Building docker image from shared lib class'
         script.sh "docker build -t salma101/$imageName ."
     }
 
-    def dockerLogin(){
+    def dockerLogin() {
         script.withCredentials([script.usernamePassword(credentialsId: 'dockerhub-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-        script.sh "echo $script.PASS| docker login -u $script.USER --password-stdin"
-    }
+            script.sh "echo $script.PASS| docker login -u $script.USER --password-stdin"
+        }
     }
 
-    def pushImage(String imageName){
+    def pushImage(String imageName) {
         script.sh "docker push salma101/$imageName"
     }
+
 }
